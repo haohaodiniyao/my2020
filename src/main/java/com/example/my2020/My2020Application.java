ad6f6e80c8e857887c1a046b3bcb4d03353ce3ef
haohaodiniyao <1270305593@qq.com>
2020-01-14 19:28:20 +0800

    初始化

---
+++ src/main/java/com/example/my2020/My2020Application.java
@@ -0,0 +1,13 @@
+package com.example.my2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class My2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(My2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my2020/My2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class My2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
